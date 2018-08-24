49e730be41a64cbc8a73c354b6d9dfa907146b62
limcuili <limcuili@hotmail.com>
2018-08-24 11:37:42 +0100

    Ignores empty value when adding to database

--- addModule/addToDatabase.js
+++ addModule/addToDatabase.js
@@ -5,8 +5,16 @@ firestore.settings(settings);
 // Initialize Cloud Firestore through Firebase
 var db = firebase.firestore();
 
+// function addSyllabus(moduleCode) {
+//   var moduleRef = db.collection("module_details")
+//   var moduleCode = document.getElementById("moduleCode").value;
+//   var topic = document.getElementsByClassName("topic").value;
+//   var syllabusArray = db.collection("module_details").doc(moduleCode).syllabus
+//   console.log(db.collection("module_details").doc("1101").data().name)
+//   syllabusArray.push(topic)
+// }
+
 function createModule() {
-  var moduleRef = db.collection("module_details")
   var code = document.getElementById("code").value;
   var name = document.getElementById("name").value;
   var lecturer = document.getElementById("lecturer").value;
@@ -32,15 +40,132 @@ function createModule() {
   var recommended_text_5 = document.getElementById("recommended_text_5").value;
   var recommended_text_6 = document.getElementById("recommended_text_6").value;
   var recommended_text_7 = document.getElementById("recommended_text_7").value;
-  moduleRef.doc(code).set({
+  var syllabusArray = new Array;
+  var recommendedTextArray = new Array;
+
+  if (syllabus_1 === "") {
+    return true;
+  } else {
+    syllabusArray.push(syllabus_1);
+  };
+
+  if (syllabus_2 === "") {
+    ;
+  } else {
+    syllabusArray.push(syllabus_2);
+  };
+
+  if (syllabus_3 === "") {
+    ;
+  } else {
+    syllabusArray.push(syllabus_3);
+  };
+
+  if (syllabus_4 === "") {
+    ;
+  } else {
+    syllabusArray.push(syllabus_4);
+  };
+
+  if (syllabus_5 === "") {
+    ;
+  } else {
+    syllabusArray.push(syllabus_5);
+  };
+
+  if (syllabus_6 === "") {
+    ;
+  } else {
+    syllabusArray.push(syllabus_6);
+  };
+
+  if (syllabus_7 === "") {
+    ;
+  } else {
+    syllabusArray.push(syllabus_7);
+  };
+
+  if (syllabus_8 === "") {
+    ;
+  } else {
+    syllabusArray.push(syllabus_8);
+  };
+
+  if (syllabus_9 === "") {
+    ;
+  } else {
+    syllabusArray.push(syllabus_9);
+  };
+
+  if (syllabus_10 === "") {
+    ;
+  } else {
+    syllabusArray.push(syllabus_10);
+  };
+
+  if (syllabus_11 === "") {
+    ;
+  } else {
+    syllabusArray.push(syllabus_11);
+  };
+
+  if (syllabus_12 === "") {
+    ;
+  } else {
+    syllabusArray.push(syllabus_12);
+  };
+
+  if (recommended_text_1 === "") {
+    ;
+  } else {
+    recommendedTextArray.push(recommended_text_1);
+  };
+
+  if (recommended_text_2 === "") {
+    ;
+  } else {
+    recommendedTextArray.push(recommended_text_2);
+  };
+
+  if (recommended_text_3 === "") {
+    ;
+  } else {
+    recommendedTextArray.push(recommended_text_3);
+  };
+
+  if (recommended_text_4 === "") {
+    ;
+  } else {
+    recommendedTextArray.push(recommended_text_4);
+  };
+
+  if (recommended_text_5 === "") {
+    ;
+  } else {
+    recommendedTextArray.push(recommended_text_5);
+  };
+
+  if (recommended_text_6 === "") {
+    ;
+  } else {
+    recommendedTextArray.push(recommended_text_6);
+  };
+
+  if (recommended_text_7 === "") {
+    ;
+  } else {
+    recommendedTextArray.push(recommended_text_7);
+  };
+
+  db.collection("module_details").doc(code).set({
     code: code,
     name: name,
     lecturer: lecturer,
     prerequisites: prerequisites,
     assessment: assessment,
     structure: structure,
-    syllabus: [syllabus_1, syllabus_2, syllabus_3, syllabus_4, syllabus_5, syllabus_6, syllabus_7, syllabus_8, syllabus_9, syllabus_10, syllabus_11, syllabus_12],
-    recommended_texts: [recommended_text_1, recommended_text_2, recommended_text_3, recommended_text_4, recommended_text_5, recommended_text_6, recommended_text_7]
+    syllabus: syllabusArray,
+    recommended_texts: recommendedTextArray
   })
   .then(function() {
     console.log("Document successfully written!");
